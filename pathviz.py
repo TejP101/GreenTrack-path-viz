1f63953052f7e3b9266722117f94659e4205715a
P Patel <ppatel@Tejs-MacBook-Air.local>
2025-07-21 15:11:22 -0400

    Made the Grid

---
+++ pathviz.py
@@ -0,0 +1,29 @@
+import pygame; 
+from sys import exit;
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock();
+pygame.display.set_caption("Path Visualizer")
+grid_size, cell_size = 40, 25
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        
+
+    screen.fill(('Grey')) 
+
+    for row in range(grid_size):
+        for col in range(grid_size):
+            x = col * cell_size
+            y = row * cell_size
+            pygame.draw.rect(screen, 'white', (x, y, cell_size, cell_size))
+            pygame.draw.rect(screen, 'black', (x, y, cell_size, cell_size), width=1)
+
+
+    clock.tick(60) 
+    pygame.display.update()
