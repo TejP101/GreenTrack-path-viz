69c80afbd2355179afb7859a8f3fe6fc7a531294
P Patel <ppatel@Tejs-MacBook-Air.local>
2025-07-23 16:07:52 -0400

    Added music

--- pathviz.py
+++ pathviz.py
@@ -4,10 +4,14 @@ from sys import exit;
 
 pygame.init()
 
+music= pygame.mixer.Sound('audio/noticed.wav')
+
 screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock();
 pygame.display.set_caption("Path Visualizer")
 grid_size, cell_size = 40, 25
+
+
 
 while True: 
     for event in pygame.event.get():
@@ -23,9 +27,9 @@ while True:
             y = row * cell_size
             pygame.draw.rect(screen, 'white', (x, y, cell_size, cell_size))
             pygame.draw.rect(screen, 'black', (x, y, cell_size, cell_size), width=1)
-
+    music.play()
 
     clock.tick(60) 
     pygame.display.update()
 
-    #Barce>Real
+    
